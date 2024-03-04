f7e90cd27d6269858aa56c89385c2f420afd5de9
Ahmad Safii <sakmadboyz@gmail.com>
2024-03-05 02:35:15 +0700

    Adding Notification Feature

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -5,7 +5,8 @@ const CONFIG = {
   DEFAULT_LANGUAGE: 'en-us',
   DATABASE_NAME: 'movie-catalogue-database',
   DATABASE_VERSION: 1,
-  OBJECT_STORE_NAME: 'movies'
+  OBJECT_STORE_NAME: 'movies',
+  WEB_SOCKET_SERVER: 'wss://movies-feed.dicoding.dev',
 }
 
 export default CONFIG
---
+++ src/scripts/utils/web-socket-initiator.js
@@ -0,0 +1,24 @@
+import CONFIG from "../globals/config"
+import NotificationHelper from "./notification-helper"
+
+const webSocketInitiator = {
+    init(url){
+        const webSocket = new WebSocket(url)
+        webSocket.onmessage = this._onMessageHandler
+    },
+
+    _onMessageHandler(message){
+        const movie = JSON.parse(message.data)
+
+        NotificationHelper.sendNotification({
+            title: `${movie.title} is on Cinema!`,
+            option: {
+                body: movie.overview,
+                image: `${CONFIG.BASE_IMAGE_URL + movie.poster_path}`
+            }
+        })
+        
+    }
+
+}
+export default webSocketInitiator
